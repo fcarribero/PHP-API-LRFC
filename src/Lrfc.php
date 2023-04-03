44ada504d1f3f07429e55a1fca35a23321cfee72
Francisco Carribero <cfrancisco@advans.mx>
2023-04-03 19:36:42 -0300

    Se corrige lógica al capturar respuesta de la API

--- src/Lrfc.php
+++ src/Lrfc.php
@@ -73,14 +73,14 @@ class Lrfc {
 
     protected function call($method, $verb = 'GET', $params = null): string {
         $verb = strtoupper($verb);
-        $url = $this->config->base_url . $method;
+        $url = $this->config->base_url . $method . ($verb == 'GET' && $params ? '?' . http_build_query($params) : '');
         $curl = curl_init();
         $postfields = null;
         if ($verb == 'POST') {
             $postfields = gettype($params) == 'array' ? json_encode($params) : $params;
         }
         curl_setopt_array($curl, [
-            CURLOPT_URL => $url . ($verb == 'GET' && $params ? '?' . http_build_query($params) : ''),
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $verb,
@@ -90,14 +90,13 @@ class Lrfc {
                 'Authorization: Bearer ' . $this->config->key
             ],
         ]);
-        if (!($result = @curl_exec($curl))) {
-            throw new LrfcException('Error de conexión');
-        }
+
+        $result = @curl_exec($curl);
         $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-        if ($http_code != 200 || $result === false) {
+        if ($http_code != 200) {
             throw new LrfcException('El servicio regresó un código de error ' . $http_code . ' ' . $result, $http_code);
         }
-        return $result;
+        curl_close($curl);
+        return json_decode($result);
     }
 }
